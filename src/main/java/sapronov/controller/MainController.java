c675e762e9bd32977a518dccdcb8ea06ac6b0c8f
AnticoreV <sapronovivancore@gmail.com>
2022-10-27 16:01:35 +0200

    Init WebForm Structure

---
+++ src/main/java/sapronov/controller/MainController.java
@@ -0,0 +1,19 @@
+package sapronov.controller;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "MainController", value = "/MainController")
+public class MainController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
