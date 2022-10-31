8c1ef71efeff58fc64cb9049933c7fd963d248fe
AnticoreV <sapronovivancore@gmail.com>
2022-10-31 10:54:15 +0100

    Validation extracted to ValidationUtil

--- src/main/java/sapronov/controller/ValidController.java
+++ src/main/java/sapronov/controller/ValidController.java
@@ -1,5 +1,7 @@
 package sapronov.controller;
 
+import sapronov.util.ValidationUtil;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -8,55 +10,14 @@ import java.io.IOException;
 
 @WebServlet(name = "ValidController", value = "/main-view")
 public class ValidController extends HttpServlet {
-    //policy_number requires minimum 8 characters
-    private boolean isMinValidLength(String pass, int length){
-        if(pass.length() < length){
-            return false;
-        }else return true;
-    }
-    //area request can have maximum 5000 characters
-    private boolean isMaxValidLength(String pass, int length){
-        if(pass.length() > length){
-            return false;
-        }else return true;
-    }
-    //policy_number requires minimum 1 small/big letter, 1 number
-    private boolean isValidSymbols(String pass){
-        int letter = 0, number = 0, big_letter = 0;
-        for(int i = 0; i < pass.length(); i++){
-            if(pass.charAt(i) > 47 && pass.charAt(i) < 58){number++;}
-            else if (pass.charAt(i) > 96 && pass.charAt(i) < 123) {letter++;}
-            else if (pass.charAt(i) > 64 && pass.charAt(i) < 91) {big_letter++;}
-            else return false;
-            }
-        if(letter == 0 || number == 0 || big_letter == 0){
-            return false;
-        }
-        return true;
-        }
-    //name and surname contains only letters
-    private boolean isValidSymbols(String name, String surname){
-        if(name == null || surname == null)return false;
-        for(int i = 0; i < name.length(); i++){
-            if (name.charAt(i) > 96 && name.charAt(i) < 123) {}
-            else if (name.charAt(i) > 64 && name.charAt(i) < 91) {}
-            else return false;
-        }
-        for(int i = 0; i < surname.length(); i++){
-            if (surname.charAt(i) > 96 && surname.charAt(i) < 123) {}
-            else if (surname.charAt(i) > 64 && surname.charAt(i) < 91) {}
-            else return false;
-        }
-        return true;
-    }
-
+    ValidationUtil valid = ValidationUtil.getInstance();
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         RequestDispatcher requestDispatcher;
-        if(!(isMinValidLength(req.getParameter("Policy_Number"),8)
-                && (isValidSymbols(req.getParameter("Policy_Number")))
-                && (isValidSymbols(req.getParameter("Name"), req.getParameter("Surname")))
-                && isMaxValidLength(req.getParameter("Area"), 5000))){
+        if(!(valid.isMinValidLength(req.getParameter("Policy_Number"),8)
+                && (valid.isValidSymbols(req.getParameter("Policy_Number")))
+                && (valid.isValidSymbols(req.getParameter("Name"), req.getParameter("Surname")))
+                && valid.isMaxValidLength(req.getParameter("Area"), 5000))){
             requestDispatcher = req.getRequestDispatcher("/index.jsp");
             requestDispatcher.forward(req, resp);
         }
---
+++ src/main/java/sapronov/util/ValidationUtil.java
@@ -0,0 +1,56 @@
+package sapronov.util;
+
+public class ValidationUtil {
+    private static ValidationUtil validationUtil;
+
+    private ValidationUtil(){}
+
+    public static ValidationUtil getInstance(){
+        if(validationUtil != null){
+            validationUtil = new ValidationUtil();
+        }
+        return validationUtil;
+    }
+
+    //policy_number requires minimum 8 characters
+    public boolean isMinValidLength(String pass, int length){
+        if(pass.length() < length){
+            return false;
+        }else return true;
+    }
+    //area request can have maximum 5000 characters
+    public boolean isMaxValidLength(String pass, int length){
+        if(pass.length() > length){
+            return false;
+        }else return true;
+    }
+    //policy_number requires minimum 1 small/big letter, 1 number
+    public boolean isValidSymbols(String pass){
+        int letter = 0, number = 0, big_letter = 0;
+        for(int i = 0; i < pass.length(); i++){
+            if(pass.charAt(i) > 47 && pass.charAt(i) < 58){number++;}
+            else if (pass.charAt(i) > 96 && pass.charAt(i) < 123) {letter++;}
+            else if (pass.charAt(i) > 64 && pass.charAt(i) < 91) {big_letter++;}
+            else return false;
+        }
+        if(letter == 0 || number == 0 || big_letter == 0){
+            return false;
+        }
+        return true;
+    }
+    //name and surname contains only letters
+    public boolean isValidSymbols(String name, String surname){
+        if(name == null || surname == null)return false;
+        for(int i = 0; i < name.length(); i++){
+            if (name.charAt(i) > 96 && name.charAt(i) < 123) {}
+            else if (name.charAt(i) > 64 && name.charAt(i) < 91) {}
+            else return false;
+        }
+        for(int i = 0; i < surname.length(); i++){
+            if (surname.charAt(i) > 96 && surname.charAt(i) < 123) {}
+            else if (surname.charAt(i) > 64 && surname.charAt(i) < 91) {}
+            else return false;
+        }
+        return true;
+    }
+}
